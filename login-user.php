5a87f8e5805592402fc2ad2744bd529fbb40bbb5
Pradeep1308 <pradeeppal156@gmail.com>
2021-05-11 23:25:06 +0530

    signup page -> taking user detail and storing in db login page ->verifying email from db. Home page -> welcome page for registered users.

---
+++ login-user.php
@@ -0,0 +1,46 @@
+<?php require_once "controllerUserData.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Login Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="form login-form">
+                <form action="login-user.php" method="POST" autocomplete="">
+                    <h2 class="text-center">Login Form</h2>
+                    <p class="text-center">Login with your email</p>
+                    <?php
+                    //cchecking email in db
+                    if(count($errors) > 0){
+                        ?>
+                        <div class="alert  text-center">
+                            <?php
+                            foreach($errors as $showerror){
+                                echo $showerror;
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <div >
+                        <input class="form-control" type="email" name="email" placeholder="Email Address" required value="<?php echo $email ?>">
+                    </div>
+                 
+                    <div >
+                        <input class="form-control button" type="submit" name="login" value="Login">
+                    </div>
+                    <div class="link login-link text-center">Not yet a member? <a href="signup-user.php">Signup now</a></div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
