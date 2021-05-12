19091e108c363a70352ada75b798ff6d1e69a7c0
Pradeep1308 <pradeeppal156@gmail.com>
2021-05-12 18:37:49 +0530

    database and control connection page -> connection to db controller page -> verify and checks all the validations in script otp page -> send otp to email for verification of email sql -> for storing user data

---
+++ controllerUserData.php
@@ -0,0 +1,104 @@
+<?php 
+session_start();
+require "connection.php";
+$email = "";
+$name = "";
+$errors = array();
+
+//if user signup button
+if(isset($_POST['signup'])){
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+  
+    $email_check = "SELECT * FROM usertable WHERE email = '$email'";
+    $res = mysqli_query($con, $email_check);
+    if(mysqli_num_rows($res) > 0){
+        $errors['email'] = "Email that you have entered is already exist!";
+    }
+    if(count($errors) === 0){
+       
+        $code = rand(999999, 111111);
+        $status = "notverified";
+        $insert_data = "INSERT INTO usertable (name, email, code, status)
+                        values('$name', '$email', '$code', '$status')";
+        $data_check = mysqli_query($con, $insert_data);
+        if($data_check){
+            $subject = "Email Verification Code";
+            $message = "Your verification code is $code";
+            $sender = "From: pradeeppal156@.com";
+            if(mail($email, $subject, $message, $sender)){
+                $info = "We've sent a verification code to your email - $email";
+                $_SESSION['info'] = $info;
+                $_SESSION['email'] = $email;
+                
+                header('location: user-otp.php');
+                exit();
+            }else{
+                $errors['otp-error'] = "Failed while sending code!";
+            }
+        }else{
+            $errors['db-error'] = "Failed while inserting data into database!";
+        }
+    }
+
+}
+    //if user click verification code submit button
+    if(isset($_POST['check'])){
+        $_SESSION['info'] = "";
+        $otp_code = mysqli_real_escape_string($con, $_POST['otp']);
+        $check_code = "SELECT * FROM usertable WHERE code = $otp_code";
+        $code_res = mysqli_query($con, $check_code);
+        if(mysqli_num_rows($code_res) > 0){
+            $fetch_data = mysqli_fetch_assoc($code_res);
+            $fetch_code = $fetch_data['code'];
+            $email = $fetch_data['email'];
+            $code = 0;
+            $status = 'verified';
+            $update_otp = "UPDATE usertable SET code = $code, status = '$status' WHERE code = $fetch_code";
+            $update_res = mysqli_query($con, $update_otp);
+            if($update_res){
+                $_SESSION['name'] = $name;
+                $_SESSION['email'] = $email;
+                header('location: home.php');
+                exit();
+            }else{
+                $errors['otp-error'] = "Failed while updating code!";
+            }
+        }else{
+            $errors['otp-error'] = "You've entered incorrect code!";
+        }
+    }
+
+    //if user click login button
+    if(isset($_POST['login'])){
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+        $check_email = "SELECT * FROM usertable WHERE email = '$email'";
+        $res = mysqli_query($con, $check_email);
+        $fetch = mysqli_fetch_assoc($res);
+        if(mysqli_num_rows($res) > 0){
+        
+                $_SESSION['email'] = $email;
+                $status = $fetch['status'];
+                if($status == 'verified'){
+                  $_SESSION['email'] = $email;
+               
+                    header('location: home.php');
+                }else{
+                    $info = "It's look like you haven't still verify your email - $email";
+                    $_SESSION['info'] = $info;
+                    header('location: user-otp.php');
+                }
+           }
+        }else{
+            $errors['email'] = "It's look like you're not yet a member! Click on the bottom link to signup.";
+        }
+    }
+    
+
+
+    
+   //if login now button click
+    if(isset($_POST['login-now'])){
+        header('Location: login-user.php');
+    }
+?>
---
+++ user-otp.php
@@ -0,0 +1,57 @@
+<?php require_once "controllerUserData.php"; ?>
+<?php 
+$email = $_SESSION['email'];
+if($email == false){
+  header('Location: login-user.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Code Verification</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+        <div class="form login-form">
+                <form action="user-otp.php" method="POST" autocomplete="off">
+                    <h2 class="text-center">Code Verification</h2>
+                    <?php 
+                    if(isset($_SESSION['info'])){
+                        ?>
+                        <div class="alert  text-center">
+                            <?php echo $_SESSION['info']; ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <?php
+                    if(count($errors) > 0){
+                        ?>
+                        <div class="alert  text-center">
+                            <?php
+                            foreach($errors as $showerror){
+                                echo $showerror;
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <div >
+                        <input class="form-control" type="number" name="otp" placeholder="Enter verification code" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="check" value="Submit">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
