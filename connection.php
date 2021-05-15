d156d3e29c7a0910e45c8d2ae72e51ba6dadd73d
Pradeep1308 <pradeeppal156@gmail.com>
2021-05-15 19:56:18 +0530

    connection to database connection page -> improved database handling, auto creation of database and table if not exist

--- connection.php
+++ connection.php
@@ -1,4 +1,18 @@
-
 <?php 
-$con = mysqli_connect('localhost', 'root', '', 'userform');
+$conn = mysqli_connect('localhost', 'root', '');
+$sql = "CREATE DATABASE IF NOT EXISTS userform";
+$conn->query($sql);
+$con= mysqli_connect('localhost', 'root', '','userform');
+$sql="CREATE TABLE IF NOT EXISTS `usertable` (
+    `id` int(11) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `code` mediumint(50) NOT NULL,
+    `status` text NOT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+$con->query($sql);
+
+
+
 ?>
